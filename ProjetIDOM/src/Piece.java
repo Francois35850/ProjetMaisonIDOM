97b763537000edb8b6b73d6fce600ebacd009346
desha <francois.deshayes@etudiant.univ-rennes1.fr>
2020-04-30 11:46:19 +0200

    Attributs et toString class Piece

--- ProjetIDOM/src/Piece.java
+++ ProjetIDOM/src/Piece.java
@@ -1,8 +1,31 @@
+import java.util.*;
 
 public class Piece {
-
-	public Piece() {
-		// TODO Auto-generated constructor stub
+	String nom;
+	List<Piece> piecesAdj;
+	List<Objet> listeObjets;
+	
+	public Piece(String nom, List<Piece> piecesAdj, List<Objet> listeObjets) {
+		this.nom = nom;
+		this.piecesAdj = piecesAdj;
+		this.listeObjets = listeObjets;
+	}
+	
+	public String toString() {
+		String res = "";
+		res+= "Vous vous trouvez dans la pièce " + this.nom + ".\n Liste des objets présents : \n";
+		ListIterator<Objet> itObjet = listeObjets.listIterator();
+		while(itObjet.hasNext()) {
+			res+= itObjet.next().toString();
+		}
+		if(piecesAdj.size()>1) {
+			res+="\nVous avez accès aux portes : ";
+			ListIterator<Piece> itPiece = piecesAdj.listIterator();
+			while(itPiece.hasNext()) {
+				res+="\n - " + itPiece.next().nom;
+			}
+		}
+		return res;
 	}
 
 }
