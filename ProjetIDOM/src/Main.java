c1d48075f807712c84364e3da2dd7f6b9accadb8
NielsCobat <nielscobat@gmail.com>
2020-04-30 11:28:15 +0200

    Commentaires ajoutés dans Main.java

--- ProjetIDOM/src/Main.java
+++ ProjetIDOM/src/Main.java
@@ -3,32 +3,40 @@ import java.util.Scanner;
 
 public class Main {
 	
+	// boolean pour l'état du jeu : true quand le jeu est en cours
 	private static boolean gameActive = true;
 	
+	// scanner pour lire les commandes du joueur
 	private static Scanner input = new Scanner(System.in);
 
+	/**
+	 * Fonction principale main
+	 */
 	public static void main(String[] args) {
 		
+		// Message de lancement du jeu
 		System.out.println("Bienvenue !");
 		System.out.println(" ");
 		
-		
+		// boucle du jeu
 		while (gameActive) {
 			
+			// Demande de la commande
 			System.out.println("Entrez une commande ('help' pour la liste des commandes) :");
 			String entree = input.nextLine();
 			
+			// code en fonction de la commande
 			switch(entree) {
-			case "quitter" :
+			case "quitter" : // commande pour quitter le jeu
 				gameActive = false;
 				System.out.println("Jeu quitté");
 				break;
-			case "help" :
+			case "help" : // commande pour afficher la liste des commandes
 				System.out.println("Liste des commandes disponibles :");
 				System.out.println("   - quitter : ferme le jeu");
 				System.out.println("   - help : affiche cette liste");
 				break;
-			default :
+			default : // cas par défaut, cad commande invalide
 				System.out.println("Commande invalide.");
 				break;
 			}
