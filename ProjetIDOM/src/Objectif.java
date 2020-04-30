10a49d5508a2c0975b97b9e58c23febb73f0d1ef
Hugo <Hugo@DESKTOP-IP5TAQQ.home>
2020-04-30 11:25:05 +0200

    Specification de objectif

--- ProjetIDOM/src/Objectif.java
+++ ProjetIDOM/src/Objectif.java
@@ -1,8 +1,18 @@
+import java.util.List;
 
 public class Objectif {
-
-	public Objectif() {
-		// TODO Auto-generated constructor stub
+	String nom;
+	int longueur;
+	List<Objectif> listeObjectifs;
+	
+	public Objectif(String nom, int lon, List<Objectif> lst) {
+		this.nom = nom;
+		longueur = lon;
+		listeObjectifs = lst;
+	}
+	
+	Boolean ObjEstTrue() {
+		return true;
 	}
 
 }
