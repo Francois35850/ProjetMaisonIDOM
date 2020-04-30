9cb20472d22db50e0edfcb789467856d1fd9f02c
NielsCobat <nielscobat@gmail.com>
2020-04-30 11:21:37 +0200

    Main.java modifié, fonctionnalités de bases implémentées.

--- ProjetIDOM/src/Main.java
+++ ProjetIDOM/src/Main.java
@@ -1,8 +1,39 @@
 
+import java.util.Scanner;
+
 public class Main {
+	
+	private static boolean gameActive = true;
+	
+	private static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Bienvenue !");
+		System.out.println(" ");
+		
+		
+		while (gameActive) {
+			
+			System.out.println("Entrez une commande ('help' pour la liste des commandes) :");
+			String entree = input.nextLine();
+			
+			switch(entree) {
+			case "quitter" :
+				gameActive = false;
+				System.out.println("Jeu quitté");
+				break;
+			case "help" :
+				System.out.println("Liste des commandes disponibles :");
+				System.out.println("   - quitter : ferme le jeu");
+				System.out.println("   - help : affiche cette liste");
+				break;
+			default :
+				System.out.println("Commande invalide.");
+				break;
+			}
+			
+		}
 
 	}
 
