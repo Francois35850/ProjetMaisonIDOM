9f59cdf0cf39b4c473ce55cf7c2aa069b03467c8
NielsCobat <nielscobat@gmail.com>
2020-04-30 12:28:04 +0200

    Joueur.java : fonctionnalitées de base

--- ProjetIDOM/src/Joueur.java
+++ ProjetIDOM/src/Joueur.java
@@ -1,12 +1,42 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Joueur {
-	
+
 	private Piece pieceCourante;
 	private List<Objet> inventaire;
 
 	public Joueur() {
-		// TODO Auto-generated constructor stub
+		this.pieceCourante = new Piece(null, null, null); // TODO
+		this.inventaire = new ArrayList<Objet>();
+	}
+
+	public List<Objet> getInventaire() {
+		return this.inventaire;
+	}
+
+	public Piece getPieceCourante() {
+		return this.pieceCourante;
+	}
+
+	public void addInventaire(Objet o) {
+		this.inventaire.add(o);
+	}
+
+	public boolean removeInventaire(Objet o) {
+		return this.inventaire.remove(o);
+	}
+
+	public static void changerPiece(String piece) {
+		switch (piece.toLowerCase()) {
+		case "":
+			// TODO : fonction qui, avec le nom, trouve la classe de la pièce
+			// TODO : si la pièce est dans la liste des pièces adjointes à la pièce
+			// courante, changer de pièce
+		default:
+			System.out.println("Pièce mal écrite ou inconnue");
+			break;
+		}
 	}
 
 }
