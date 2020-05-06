c6b3a53b12ae7a724bb1af4a6bb80dd418db2bbd
Hugo <Hugo@DESKTOP-IP5TAQQ.home>
2020-05-06 15:48:51 +0200

    Ajout de fonctions addObjets et soustractObjets à Piece Ajout du paramètres est au rez de chaussée à Piece

--- ProjetIDOM/src/Piece.java
+++ ProjetIDOM/src/Piece.java
@@ -2,29 +2,48 @@ import java.util.*;
 
 public class Piece {
 	String nom;
+	Boolean estAuRezDeC;
 	List<Piece> piecesAdj;
 	List<Objet> listeObjets;
 	
-	public Piece(String nom, List<Piece> piecesAdj, List<Objet> listeObjets) {
+	public Piece(String nom, List<Piece> piecesAdj, List<Objet> listeObjets, boolean etage) {
 		this.nom = nom;
 		this.piecesAdj = piecesAdj;
 		this.listeObjets = listeObjets;
+		estAuRezDeC = etage;
+	}
+	
+	public void addObjets(Objet o) {
+		listeObjets.add(o);
+	}
+	
+	public void soustractObjets(Objet o) {
+		boolean retire = false;
+		ListIterator<Objet> itObjet = listeObjets.listIterator();
+		while (!retire | itObjet.hasNext()) {
+			Objet next = itObjet.next();
+			if (next == o) {
+				itObjet.remove();
+				retire = true;
+			}
+		}
 	}
 	
 	public String toString() {
 		String res = "";
-		res+= "Vous vous trouvez dans la pièce " + this.nom + ".\n Liste des objets présents : \n";
+		res+= "Vous vous trouvez dans la piï¿½ce " + this.nom + ".\n Liste des objets prï¿½sents : \n";
 		ListIterator<Objet> itObjet = listeObjets.listIterator();
 		while(itObjet.hasNext()) {
 			res+= itObjet.next().toString();
 		}
 		if(piecesAdj.size()>1) {
-			res+="\nVous avez accès aux portes : ";
+			res+="\nVous avez accï¿½s aux portes : ";
 			ListIterator<Piece> itPiece = piecesAdj.listIterator();
 			while(itPiece.hasNext()) {
 				res+="\n - " + itPiece.next().nom;
 			}
 		}
+		else res+= "\nVous avez accÃ¨s Ã  la porte " + piecesAdj.get(0).nom;
 		return res;
 	}
 
