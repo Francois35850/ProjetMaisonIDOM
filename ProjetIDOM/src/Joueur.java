573db0a3427f2de8e9436d6e1cd36871104f239c
NielsCobat <nielscobat@gmail.com>
2020-05-07 09:08:28 +0200

    Changement de Joueur.java en static

--- ProjetIDOM/src/Joueur.java
+++ ProjetIDOM/src/Joueur.java
@@ -1,54 +1,62 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.StringTokenizer;
 
 public class Joueur {
 
 	private static Piece pieceCourante;
-	private List<Objet> inventaire;
+	private static List<Objet> inventaire;
 
-	public Joueur(Piece debut) {
-		this.pieceCourante = debut;
-		this.inventaire = new ArrayList<Objet>();
+	static {
+		init();
+	}
+
+	private Joueur() {
+	}
+
+	private static void init() {
+		pieceCourante = null;
+		inventaire = new ArrayList<Objet>();
 	}
 
 	public List<Objet> getInventaire() {
-		return this.inventaire;
+		return inventaire;
 	}
 
 	public static Piece getPieceCourante() {
 		return pieceCourante;
 	}
-	
+
 	public static void setPieceCourante(Piece p) {
 		pieceCourante = p;
 	}
 
 	public void addInventaire(Objet o) {
-		this.inventaire.add(o);
+		inventaire.add(o);
 	}
 
 	public boolean removeInventaire(Objet o) {
-		return this.inventaire.remove(o);
+		return inventaire.remove(o);
 	}
-	
+
 	public static Piece StringToPiece(String nomPiece) {
 		ListIterator<Piece> it = pieceCourante.piecesAdj.listIterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			Piece nextPiece = it.next();
-			if(nextPiece.nom.compareToIgnoreCase(nomPiece)==0) return nextPiece;
+			if (nextPiece.nom.compareToIgnoreCase(nomPiece) == 0)
+				return nextPiece;
 		}
 		return null;
 	}
 
 	public static void changerPiece(String piece) {
 		Piece deplacement = StringToPiece(piece);
-		if(deplacement == null) System.out.println("Désolé je n'ai pas compris dans quelle pièce vous voulez aller...");
+		if (deplacement == null)
+			System.out.println("Désolé je n'ai pas compris dans quelle pièce vous voulez aller...");
 		else {
 			setPieceCourante(deplacement);
 			System.out.println(getPieceCourante().toString());
 		}
 	}
-	
+
 }
