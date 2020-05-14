337a7165d24b263a702d457221cebd18f99842ed
NielsCobat <nielscobat@gmail.com>
2020-05-14 15:21:32 +0200

    Ajout de Alarme.java

--- ProjetIDOM/src/objets/Alarme.java
+++ ProjetIDOM/src/objets/Alarme.java
@@ -1,4 +1,18 @@
+package objets;
 
-public class Alarme {
+import main.Objet;
+
+public final class Alarme extends Objet {
+
+	public Alarme(String nom) {
+		super(nom);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String etat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
