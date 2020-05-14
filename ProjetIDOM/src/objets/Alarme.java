c3ab46d45db0166b65fd4e414719c1fc7e204992
Ilias Tiburce <tiburceilias@gmail.com>
2020-05-14 15:23:40 +0200

    reglage conflit

--- ProjetIDOM/src/objets/Alarme.java
+++ ProjetIDOM/src/objets/Alarme.java
@@ -1,4 +1,31 @@
+package objets;
 
-public class Alarme {
+import main.Objet;
 
+public final class Alarme extends Objet {
+
+	public DetecteurOuverture(String nom, int fonctionnement) {
+		this.nom=nom;
+		this.fonctionnement=fonctionnement;
+	}
+	
+	public DetecteurPresence(String nom, int fonctionnement) {
+		this.nom=nom;
+		this.fonctionnement=fonctionnement;
+	}
+	public Digicode(String nom, int code) {
+		this.nom=nom;
+		this.code=code;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
