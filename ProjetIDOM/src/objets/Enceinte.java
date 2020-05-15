f3dd516b26f87ff60ee0c47aaa0625e56743c80e
desha <francois.deshayes@etudiant.univ-rennes1.fr>
2020-05-15 14:21:46 +0200

    Enlever enceintes

--- ProjetIDOM/src/objets/Enceinte.java
+++ ProjetIDOM/src/objets/Enceinte.java
@@ -1,4 +1,18 @@
+package objets;
 
-public class Enceinte {
+import main.Objet;
+
+public final class Enceinte extends Objet {
+
+	public Enceinte(String nom) {
+		super(nom);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String etat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
