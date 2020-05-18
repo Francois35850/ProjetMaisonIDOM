d36f66ed9a8dde56f86078853c6dd17da41579e1
NielsCobat <nielscobat@gmail.com>
2020-05-18 15:25:23 +0200

    Résolution de bugs

--- ProjetIDOM/src/main/Main.java
+++ ProjetIDOM/src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+
 import java.util.*;
 import java.util.Scanner;
 
@@ -17,42 +18,42 @@ public class Main {
 	private static Scanner input = new Scanner(System.in);
 
 	private ITelephone iPhone = ITelephone.newTelephone();
-	
+
 	// Ajout des pièces
-	private static Piece garage = new Piece("Garage",true);
-	private static Piece cuisine = new Piece("Cuisine",true);
-	private static Piece salon = new Piece("Salon",  true);
-	private static Piece salle_a_manger = new Piece("Salle a manger",true);
-	private static Piece buanderie = new Piece("Buanderie",true);
-	private static Piece entree = new Piece("Entree",  true);
+	private static Piece garage = new Piece("Garage", true);
+	private static Piece cuisine = new Piece("Cuisine", true);
+	private static Piece salon = new Piece("Salon", true);
+	private static Piece salle_a_manger = new Piece("Salle a manger", true);
+	private static Piece buanderie = new Piece("Buanderie", true);
+	private static Piece entree = new Piece("Entree", true);
 	private static Piece chambre1 = new Piece("Chambre de Régine", true);
-	private static Piece chambre2 = new Piece("Chambre de David",  true);
-	private static Piece chambre_parentale = new Piece("Chambre parentale",  false);
-	private static Piece sdb1 = new Piece("Salle de bain",  true);
+	private static Piece chambre2 = new Piece("Chambre de David", true);
+	private static Piece chambre_parentale = new Piece("Chambre parentale", false);
+	private static Piece sdb1 = new Piece("Salle de bain", true);
 	private static Piece sdb2 = new Piece("Salle de bain", false);
 	private static Piece bureau = new Piece("Bureau", true);
-	private static Piece salle_de_jeux = new Piece("Salle de jeux",  false);
+	private static Piece salle_de_jeux = new Piece("Salle de jeux", false);
 	private static Piece couloir = new Piece("Couloir", false);
 	private static Piece grenier = new Piece("Grenier", false);
-	private static Piece studio = new Piece("Studio",  false);
+	private static Piece studio = new Piece("Studio", false);
 	private static Piece dressing = new Piece("Dressing", false);
-	private static Piece toilettes = new Piece("Toilettes",  false);
-	private static Piece jardin = new Piece("Jardin",  true);
+	private static Piece toilettes = new Piece("Toilettes", false);
+	private static Piece jardin = new Piece("Jardin", true);
 
 	// Ajout des objets
 	private static Objets enceinteSalon = new Enceinte("Enceinte salon", false);
-	private static Objets frigo = new Frigo("Frigo",false);
-	private static Objets canapeSalon = new Canape("Canapé salon",false);
+	private static Objets frigo = new Frigo("Frigo", false);
+	private static Objets canapeSalon = new Canape("Canapé salon", false);
 	private static Objets televisionSalon = new Television("TV salon");
 	private static Objets televisionSalleDeJeux = new Television("TV salle de jeux");
-	private static Objets telecommandeSalon = new Telecommande("Télécommande salon",false,true,0,0);
-	private static Objets telecommandeSalleDeJeux = new Telecommande("Télécommande sdj",false,true,4,2);
-	
+	private static Objets telecommandeSalon = new Telecommande("Télécommande salon", false, true, 0, 0);
+	private static Objets telecommandeSalleDeJeux = new Telecommande("Télécommande sdj", false, true, 4, 2);
+
 	/**
 	 * Fonction principale main
 	 */
 	public static void main(String[] args) {
-		//Création des listes de pièces adjacentes
+		// Création des listes de pièces adjacentes
 		Piece[] adjGarage = { buanderie, cuisine };
 		Piece[] adjCuisine = { buanderie, garage, salle_a_manger };
 		Piece[] adjBuanderie = { cuisine, garage };
@@ -72,13 +73,13 @@ public class Main {
 		Piece[] adjGrenier = { couloir };
 		Piece[] adjDressing = { chambre_parentale };
 		Piece[] adjJardin = { salon };
-		
+
 		// Création des listes d'objets
 		Objets[] objGarage = {};
-		Objets[] objCuisine = {frigo};
+		Objets[] objCuisine = { frigo };
 		Objets[] objBuanderie = {};
 		Objets[] objSalleAManger = {};
-		Objets[] objSalon = {enceinteSalon,canapeSalon,televisionSalon,telecommandeSalon};
+		Objets[] objSalon = { enceinteSalon, canapeSalon, televisionSalon, telecommandeSalon };
 		Objets[] objEntree = {};
 		Objets[] objChambre1 = {};
 		Objets[] objChambre2 = {};
@@ -86,80 +87,81 @@ public class Main {
 		Objets[] objBureau = {};
 		Objets[] objCouloir = {};
 		Objets[] objSdB2 = {};
-		Objets[] objSalleDeJeux = {televisionSalleDeJeux};
+		Objets[] objSalleDeJeux = { televisionSalleDeJeux };
 		Objets[] objChambreParentale = {};
 		Objets[] objStudio = {};
 		Objets[] objToilettes = {};
 		Objets[] objGrenier = {};
 		Objets[] objDressing = {};
 		Objets[] objJardin = {};
-		
+
 		// Ajout des pièces adj et des objets
 		garage.ajouterPiecesAdj(adjGarage);
 		garage.ajouterObjets(objGarage);
-		
+
 		cuisine.ajouterPiecesAdj(adjCuisine);
 		cuisine.ajouterObjets(objCuisine);
-		
+
 		buanderie.ajouterPiecesAdj(adjBuanderie);
 		buanderie.ajouterObjets(objBuanderie);
-		
+
 		salle_a_manger.ajouterPiecesAdj(adjSalleAManger);
 		salle_a_manger.ajouterObjets(objSalleAManger);
-		
+
 		salon.ajouterPiecesAdj(adjSalon);
 		salon.ajouterObjets(objSalon);
-		
+
 		entree.ajouterPiecesAdj(adjEntree);
 		entree.ajouterObjets(objEntree);
-		
+
 		chambre1.ajouterPiecesAdj(adjChambre1);
 		chambre1.ajouterObjets(objChambre1);
-		
+
 		chambre2.ajouterPiecesAdj(adjChambre2);
 		chambre2.ajouterObjets(objChambre2);
-		
+
 		sdb1.ajouterPiecesAdj(adjSdB1);
 		sdb1.ajouterObjets(objSdB1);
-		
+
 		bureau.ajouterPiecesAdj(adjBureau);
 		bureau.ajouterObjets(objBureau);
-		
+
 		couloir.ajouterPiecesAdj(adjCouloir);
 		couloir.ajouterObjets(objCouloir);
-		
+
 		sdb2.ajouterPiecesAdj(adjSdB2);
 		sdb2.ajouterObjets(objSdB2);
-		
+
 		salle_de_jeux.ajouterPiecesAdj(adjSalleDeJeux);
 		salle_de_jeux.ajouterObjets(objSalleDeJeux);
-		
+
 		chambre_parentale.ajouterPiecesAdj(adjChambreParentale);
 		chambre_parentale.ajouterObjets(objChambreParentale);
-		
+
 		studio.ajouterPiecesAdj(adjStudio);
 		studio.ajouterObjets(objStudio);
-		
+
 		toilettes.ajouterPiecesAdj(adjToilettes);
 		toilettes.ajouterObjets(objToilettes);
-		
+
 		grenier.ajouterPiecesAdj(adjGrenier);
 		grenier.ajouterObjets(objGrenier);
-		
+
 		dressing.ajouterPiecesAdj(adjDressing);
 		dressing.ajouterObjets(objDressing);
-		
+
 		jardin.ajouterPiecesAdj(adjJardin);
 		jardin.ajouterObjets(objJardin);
-		
 
 		Joueur.setPieceCourante(chambre_parentale);
 
 		// Message de lancement du jeu
 		System.out.println("Bonjour !");
 		System.out.println("Vous venez de vous réveiller, il est déjà 13h05...");
-		System.out.println("Il va falloir se dépêcher, vous avez un rendez-vous à 18h30 et il vous faut partir de chez vous avant 18h15.");
-		System.out.println("Mais vous avez encore beaucoup à faire avant de sortir ! Voici la liste des tâches que vous devez accomplir :");
+		System.out.println(
+				"Il va falloir se dépêcher, vous avez un rendez-vous à 18h30 et il vous faut partir de chez vous avant 18h15.");
+		System.out.println(
+				"Mais vous avez encore beaucoup à faire avant de sortir ! Voici la liste des tâches que vous devez accomplir :");
 		System.out.println();
 		System.out.println(Joueur.getPieceCourante().toString());
 		System.out.println(" ");
@@ -199,24 +201,26 @@ public class Main {
 				Telephone.utiliserTelephone();
 				break;
 			case "UTILISER": // commande pour utiliser un objet
-				String resObjet = "";
-				resObjet = entreeSplit[1];
-				if (entreeSplit.length > 2) {
-					for (int i = 2; i < entreeSplit.length; i++) {
-						resObjet += " ";
-						resObjet += entreeSplit[i];
+				if (entreeSplit.length > 1) {
+					String resObjet = "";
+					resObjet = entreeSplit[1];
+					if (entreeSplit.length > 2) {
+						for (int i = 2; i < entreeSplit.length; i++) {
+							resObjet += " ";
+							resObjet += entreeSplit[i];
+						}
 					}
+					Objets cherche = Joueur.StringtoObjet(resObjet);
+					if (cherche != null) {
+						cherche.utiliserObjet();
+					} else
+						System.out.println("Cet objet n'est pas disponible ... \n");
 				}
-				Objets cherche = Joueur.StringtoObjet(resObjet);
-				if (cherche!=null) {
-					cherche.utiliserObjet();
-				}
-				else System.out.println("Cet objet n'est pas disponible ... \n");
 				break;
 			case "INVENTAIRE": // commande pour consulter l'inventaire
 				Joueur.inventaireToString();
 				break;
-			case "INFOS" :
+			case "INFOS":
 				System.out.println(Joueur.getPieceCourante().toString());
 				break;
 			case "SORTIR": // commande pour sortir de la maison depuis l'entrée
@@ -224,8 +228,8 @@ public class Main {
 					System.out.println("Vous êtes sorti de la maison");
 					gameActive = false;
 					System.out.println("Jeu terminé");
-				}
-				else System.out.println("Vous ne pouvez sortir que depuis l'entrée");
+				} else
+					System.out.println("Vous ne pouvez sortir que depuis l'entrée");
 				break;
 			default: // cas par défaut, cad commande invalide
 				System.out.println("Commande invalide.");
@@ -235,6 +239,5 @@ public class Main {
 		}
 
 	}
-
 
 }
