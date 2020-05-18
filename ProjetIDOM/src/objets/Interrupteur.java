a28148864c6734a03e47298ff23094f41611c44c
desha <francois.deshayes@etudiant.univ-rennes1.fr>
2020-05-18 14:45:05 +0200

    Interrupteur / Lumière

---
+++ ProjetIDOM/src/objets/Interrupteur.java
@@ -0,0 +1,36 @@
+package objets;
+
+import java.util.Scanner;
+
+import main.ObjetNonRamassable;
+
+public final class Interrupteur extends ObjetNonRamassable {
+
+	private boolean allume;
+	
+	public Interrupteur(String nom, boolean allume) {
+		super(nom);
+		this.allume = allume;
+	}
+
+	@Override
+	public String etat() {
+		String res = "";
+		if(allume) res += " : lumière allumée";
+		else res += " : lumière éteinte";
+		return res + "\n";
+	}
+
+	@Override
+	public void utiliserObjet() {
+		if(allume) {
+			System.out.println("Vous avez éteint la lumière");
+			allume = false;
+		}
+		else {
+			System.out.println("Vous avez allumé la lumière");
+			allume = true;
+		}
+	}
+
+}
