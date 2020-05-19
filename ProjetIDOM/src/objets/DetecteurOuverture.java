522189dffbb435d8abb901aeb8d5ad63904fa406
Ilias Tiburce <tiburceilias@gmail.com>
2020-05-19 15:47:41 +0200

    creation do

---
+++ ProjetIDOM/src/objets/DetecteurOuverture.java
@@ -0,0 +1,34 @@
+package objets;
+
+import main.ObjetNonRamassable;
+
+public class DetecteurOuverture extends ObjetNonRamassable {
+	
+	private boolean ouverture;
+
+	public DetecteurOuverture(String nom, boolean ouverture) {
+		super(nom);
+		this.ouverture = ouverture;
+	}
+	
+	public String etat() {
+		String res = "";
+		if(this.ouverture==true) {res+= " : détecteur ouvert";}
+		else {res += " : fdétecteur fermé";}
+		return res + "\n";
+	}
+	
+	public String toString() {
+		String res = "";
+		res += " - " + super.getNom() + " " + this.etat();
+		return res;
+	}
+	
+	public void setEtat(Fenetre fenetre) {
+		if(getEtat(fenetre)==true) {this.ouverture=true;}
+		else {this.ouverture=false;}
+		}
+		
+		
+	public boolean getEtat(DetecteurOuverture detecteurOuverture) {return this.ouverture;}
+}
