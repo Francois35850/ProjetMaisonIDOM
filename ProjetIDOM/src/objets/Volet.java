bb4d2f56f4485fa1a91ebd5144717a678772295e
desha <francois.deshayes@etudiant.univ-rennes1.fr>
2020-05-19 14:55:34 +0200

    Volets

--- ProjetIDOM/src/objets/Volet.java
+++ ProjetIDOM/src/objets/Volet.java
@@ -2,19 +2,21 @@ package objets;
 
 import java.util.Scanner;
 
-public class Volet {
-	
-	//Attributs
+import main.ObjetNonRamassable;
+
+public class Volet extends ObjetNonRamassable {
+
+	// Attributs
 	private boolean etat_volet;
-	
-	//Constructeur
+
+	// Constructeur
 	public Volet(String nom, boolean etat_volet) {
 		super(nom);
-		this.etat_volet=etat_volet;
+		this.etat_volet = etat_volet;
 	}
-	
-	//fonction qui initialise les fenetre de la maison
-	public static void initialisationFenetreMaison(){
+
+	// fonction qui initialise les fenetre de la maison
+	public static void initialisationFenetreMaison() {
 		Volet volet_bureau = new Volet("volet_bureau", false, false);
 		Volet volet_salon1 = new Volet("volet__salon1", false, false);
 		Volet volet_salon2 = new Volet("volet__salon2", false, false);
@@ -25,61 +27,57 @@ public class Volet {
 		Volet volet_studio = new Volet("volet_studio", false, false);
 		Volet volet_cuisine = new Volet("volet_cuisine", false, false);
 		Volet volet_sallemanger = new Volet("volet_sallemanger", false, false);
-		}
-	
+	}
+
 	public String etat() {
 		String res = "";
-		if(this.etat_volet==true) {res+=" : ouvert";}
-		else {res+=" : fermé";}
+		if (this.etat_volet == true) {
+			res += " : ouvert";
+		} else {
+			res += " : fermé";
+		}
 		return res + "\n";
 	}
-	
-	//fonction qui permet d'utiliser la fenetre 			
+
+	// fonction qui permet d'utiliser la fenetre
 	public void utiliserObjet() {
 		this.setUtilisation(true);
 		Scanner input = new Scanner(System.in);
-		System.out.println("Vous utilisez " + this.getNom() +".");
-		while(this.getUtilisation()==true){
-			System.out.println("Liste des actions possibles pour cet objet :\n  - ouvrir le volet\n  - fermer le volet\n  - retour");
+		System.out.println("Vous utilisez " + this.getNom() + ".");
+		while (this.getUtilisation() == true) {
+			System.out.println(
+					"Liste des actions possibles pour cet objet :\n  - ouvrir le volet\n  - fermer le volet\n  - retour");
 			String entree = input.nextLine();
 			switch (entree) {
-				case "OUVRIR":
-					setEtat(true);
-					break;
-				case "FERMER":
-					setEtat(false);
-					break;
-				case "RETOUR" :
-					this.setUtilisation(false);
-					System.out.println("Vous arrêtez d'utiliser l'objet");
-					break;	
-				default :
-					System.out.println("La commande n'est pas valide");
-					break;
+			case "OUVRIR":
+				setEtat(true);
+				break;
+			case "FERMER":
+				setEtat(false);
+				break;
+			case "RETOUR":
+				this.setUtilisation(false);
+				System.out.println("Vous arrêtez d'utiliser l'objet");
+				break;
+			default:
+				System.out.println("La commande n'est pas valide");
+				break;
 			}
+		}
+	}
 
 	public String toString() {
 		String res = "";
 		res += " - " + super.getNom() + " " + this.etat();
 		return res;
 	}
-	
-	public boolean getEtat(Volet volet) {return this.etat_volet;}
-	
-	public void setEtat(boolean use) {this.etat_volet = use;}
-	
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public boolean getEtat(Volet volet) {
+		return this.etat_volet;
+	}
+
+	public void setEtat(boolean use) {
+		this.etat_volet = use;
+	}
+
 }
