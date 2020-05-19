3b2755144ccbf1257312262b5e2576b898b1dec3
Hugo <Hugo@DESKTOP-IP5TAQQ.home>
2020-05-19 17:04:49 +0200

    ObjEstTrue

--- ProjetIDOM/src/main/Objectif.java
+++ ProjetIDOM/src/main/Objectif.java
@@ -1,19 +1,26 @@
 package main;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Objectif {
 	private String nom;
 	private int longueur;
-	private List<Objectif> listeObjectifs;
+	private List<Boolean> listeObjectifs;
 	
-	public Objectif(String nom, int lon, List<Objectif> lst) {
+	public Objectif(String nom, int lon, List<Boolean> lst) {
 		this.nom = nom;
 		longueur = lon;
 		listeObjectifs = lst;
 	}
 	
-	Boolean ObjEstTrue() {
-		return true;
+	public boolean ObjEstTrue() {
+		boolean realise = true;
+		ListIterator<Boolean> itObjet = listeObjectifs.listIterator();
+		while (itObjet.hasNext() && realise) {
+			boolean prochain = itObjet.next();
+			if (!prochain) realise = false;
+		}
+		return realise;
 	}
 
 }
