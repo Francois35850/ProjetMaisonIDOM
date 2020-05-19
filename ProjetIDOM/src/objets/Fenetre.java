893e92323f870ec0153773dc916d8bb2e9c6faac
Ilias Tiburce <tiburceilias@gmail.com>
2020-05-19 14:22:39 +0200

    création volet et fenetre, modification studio et reglage conflits alarme

---
+++ ProjetIDOM/src/objets/Fenetre.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Fenetre extends ObjetNonRamassable{
+
+	// scanner pour lire les commandes du joueur
+	private static Scanner input = new Scanner(System.in);
+	
+	//Attributs
+	private boolean etat_fenetre;
+	private boolean utilisation;
+
+	//Constructeur
+	public Fenetre(String nom, boolean utilisation, boolean etat_fenetre) {
+		super();
+		this.utilisation=utilisation;
+		this.etat_fenetre=etat_fenetre;
+	}
+	
+	//fonction qui initialise les fenetre de la maison
+	public static void initialisationFenetreMaison(){
+		Fenetre fenetre_bureau = new Fenetre("fenetre_bureau", false, false);
+		Fenetre fenetre_salon1 = new Fenetre("fenetre__salon1", false, false);
+		Fenetre fenetre_salon2 = new Fenetre("fenetre__salon2", false, false);
+		Fenetre fenetre_ch1 = new Fenetre("fenetre_ch1", false, false);
+		Fenetre fenetre_ch2 = new Fenetre("fenetre_ch2", false, false);
+		Fenetre fenetre_chp = new Fenetre("fenetre_chp", false, false);
+		Fenetre fenetre_sallejeux = new Fenetre("fenetre_sallejeux", false, false);
+		Fenetre fenetre_studio = new Fenetre("fenetre_studio", false, false);
+		Fenetre fenetre_cuisine = new Fenetre("fenetre_cuisine", false, false);
+		Fenetre fenetre_sallemanger = new Fenetre("fenetre_sallemanger", false, false);
+	}
+	
+	//fonction qui retourne l'état de la fenetre 		
+	public String etat(Fenetre fenetre)	{
+		String res ="";
+		if(this.etat_fenetre==true) {res+=" : ouvert";}
+		else {res+=" : fermé";}
+		return res + "\n";
+	}
+	
+
+	//fonction qui permet d'utiliser la fenetre 			
+	public void utiliserObjet() {
+		utilisation=true;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Vous utilisez " + this.getNom() +".");
+		while(utilisation){
+			System.out.println("Liste des actions possibles pour cet objet :\n  - ouvrir la fenetre/le volet\n  - fermer la fenetre/le volet\n  - retour");
+			String entree = input.nextLine();
+			switch (entree) {
+			case "OUVRIR":
+				setEtat(true);
+				break;
+			case "FERMER":
+				setEtat(false);
+				break;
+			case "RETOUR" :
+				this.setUtilisation(false);
+				System.out.println("Vous arrêtez d'utiliser l'objet");
+				break;	
+			default :
+				System.out.println("La commande n'est pas valide");
+				break;
+			}
+	}
+
+	public String toString() {
+		String res = "";
+		res += " - " + super.getNom() + " " + this.etat();
+		return res;
+	}
+	
+	public boolean getEtat(Fenetre fenetre) {return this.etat_fenetre;}
+	
+	public void setUtilisation(boolean use){this.utilisation=use;}
+	
+	public void setEtat(boolean use) {this.etat_fenetre = use;}
+	
+	public String getNom() {return super.getNom();}
+}
+}
---
+++ ProjetIDOM/src/objets/Volet.java
@@ -0,0 +1,87 @@
+
+public class Volet {
+	
+	//Attributs
+	private boolean etat_volet
+	private boolean utilisation;
+	
+	//Constructeur
+	public Volet(String nom, boolean utilisation, boolean etat_volet) {
+		super();
+		this.utilisation=utilisation;
+		this.etat_volet=etat_volet;
+	}
+	
+	//fonction qui initialise les fenetre de la maison
+	public static void initialisationFenetreMaison(){
+		Volet volet_bureau = new Volet("volet_bureau", false, false);
+		Volet volet_salon1 = new Volet("volet__salon1", false, false);
+		Volet volet_salon2 = new Volet("volet__salon2", false, false);
+		Volet volet_ch1 = new Volet("volet_ch1", false, false);
+		Volet volet_ch2 = new Volet("volet_ch2", false, false);
+		Volet volet_chp = new Volet("volet_chp", false, false);
+		Volet volet_sallejeux = new Volet("volet_sallejeux", false, false);
+		Volet volet_studio = new Volet("volet_studio", false, false);
+		Volet volet_cuisine = new Volet("volet_cuisine", false, false);
+		Volet volet_sallemanger = new Volet("volet_sallemanger", false, false);
+		}
+	
+	public String etat(Volet volet) {
+		String res = "";
+		if(this.etat_volet==true) {res+=" : ouvert";}
+		else {res+=" : fermé";}
+		return res + "\n";
+	}
+	
+	//fonction qui permet d'utiliser la fenetre 			
+	public void utiliserObjet(Volet volet) {
+		setUtilisation(true);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Vous utilisez " + this.getNom() +".");
+		while(this.utilisation==true){
+			System.out.println("Liste des actions possibles pour cet objet :\n  - ouvrir le volet\n  - fermer le volet\n  - retour");
+			String entree = input.nextLine();
+			switch (entree) {
+				case "OUVRIR":
+					setEtat(true);
+					break;
+				case "FERMER":
+					setEtat(false);
+					break;
+				case "RETOUR" :
+					this.setUtilisation(false);
+					System.out.println("Vous arrêtez d'utiliser l'objet");
+					break;	
+				default :
+					System.out.println("La commande n'est pas valide");
+					break;
+			}
+
+	public String toString() {
+		String res = "";
+		res += " - " + super.getNom() + " " + this.etat();
+		return res;
+	}
+	
+	public boolean getEtat(Volet volet) {return this.etat_volet;}
+	
+	public void setUtilisation(boolean use){this.utilisation = use;}
+	
+	public void setEtat(boolean use) {this.etat_volet = use;}
+	
+	public String getNom() {return super.getNom();}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
