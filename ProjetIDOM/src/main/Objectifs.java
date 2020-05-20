e55c184b9028c50d1e8feab134a1d0d949130c72
NielsCobat <nielscobat@gmail.com>
2020-05-20 15:17:13 +0200

    Changement structure Objectif.java

---
+++ ProjetIDOM/src/main/Objectifs.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.function.Function;
+
+public class Objectifs {
+
+	List<Objectif> objectifs;
+
+	public class Objectif {
+
+		private String nom;
+		private Function<Integer, Boolean> succes;
+
+		public Objectif(String nom, Function<Integer, Boolean> succes) {
+			this.nom = nom;
+			this.succes = succes;
+		}
+
+		public boolean estRempli() {
+			return this.succes.apply(0);
+		}
+
+		public String toString() {
+			return this.nom;
+		}
+
+	}
+
+	public Objectifs(String nom) {
+		this.objectifs = new ArrayList<Objectif>();
+
+		this.objectifs.add(new Objectif("test", (a) -> testObjectif(a)));
+	}
+
+	public boolean allObjectifsComplets() {
+		for (Objectif o : this.objectifs) {
+			if (!o.estRempli())
+				return false;
+		}
+		return true;
+	}
+
+	private boolean testObjectif(int a) {
+		return true;
+	}
+
+}
