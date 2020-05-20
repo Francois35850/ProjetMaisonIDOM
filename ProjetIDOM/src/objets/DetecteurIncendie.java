e2b3ea99d6abf369ba947bd437ea17313f017655
Ilias Tiburce <tiburceilias@gmail.com>
2020-05-20 22:13:15 +0200

    avancement

--- ProjetIDOM/src/objets/DetecteurIncendie.java
+++ ProjetIDOM/src/objets/DetecteurIncendie.java
@@ -5,12 +5,14 @@ import main.ObjetNonRamassable;
 public class DetecteurIncendie extends ObjetNonRamassable {
 
 	private int etat;
+	private int pourcentage_pile;
 	private int pile;
 
-	public DetecteurIncendie(String nom, int etat, int pile) {
+	public DetecteurIncendie(String nom, int etat, int pourcentage_pile, int pile) {
 		super(nom);
 		this.etat = etat;
-		this.pile = pile;
+		this.pile=pile;
+		this.pourcentage_pile = pourcentage_pile;
 	}
 
 	public String etat() {
@@ -18,7 +20,7 @@ public class DetecteurIncendie extends ObjetNonRamassable {
 		if (this.etat == 1) {
 			res += " : incendie en cours";
 		} else {
-			res += " : pas de fumée";
+			res += " : pas de fumée d�tect�e";
 		}
 		return res + "\n";
 	}
@@ -29,22 +31,39 @@ public class DetecteurIncendie extends ObjetNonRamassable {
 		return res;
 	}
 
-	public void setEtat(int use) {
-		this.etat = use;
-	}
+	public void setEtat(int use) {this.etat = use;}
+	
+	public int getPourcentage() {return this.pourcentage_pile;}
+	
+	public int getPile() {return this.pile;}
 
 	public void getBatterie() {
-		if (this.pile == 0) {
-			System.out.println("Batterie du détecteur incendie vide");
-		} else {
-			System.out.println("Batterie du détecteur incendie pleine");
-		}
+		if (this.pile == 0) {System.out.println("Batterie du détecteur incendie vide");} 
+		else {System.out.println("Batterie du détecteur incendie � :" + getPile() + "%");}
 	}
 
-	@Override
 	public void utiliserObjet() {
-		// TODO Auto-generated method stub
+		this.setUtilisation(true);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Vous utilisez " + this.getNom() +".");
+		while(this.getUtilisation()==true){
+			System.out.println("Liste des actions possibles pour cet objet :\n  - voir pourcentage batterie\n  - changer la pile \n  - retour");
+			String entree = input.nextLine();
+			switch (entree.toUpperCase()) {
+				case "VOIR POURCENTAGE BATTERIE":
+					getBatterie();
+				case "CHANGER LA PILE"
+					
+				case "RETOUR" :
+					this.setUtilisation(false);
+					System.out.println("Vous arrêtez d'utiliser l'objet");
+					break;	
+				default :
+					System.out.println("La commande n'est pas valide");
+					break;
+			}
+		}
 
 	}
-
+	
 }
